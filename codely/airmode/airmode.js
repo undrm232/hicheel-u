e5fa6deeb5921195186b29080b128ce4ac3b1691
undrm232 <undramdarkhanbaatar@gmail.com>
2024-07-26 12:35:48 +0800

    13

---
+++ codely/airmode/airmode.js
@@ -0,0 +1,15 @@
+
+// init ball and set h1 & h2 as models (default class)
+
+
+// init h1 with airMode
+const h1 = new Cotton('#image1', {
+  airMode: true
+})
+
+// init h2 with airMode reverse
+const h2 = new Cotton('#image2', {
+  airMode: {
+    reverse: true
+  }
+})
---
+++ codely/cursor/cursor.js
@@ -0,0 +1,8 @@
+// init ball
+const ball = new Cotton('#ball', {
+  speed: 1
+})
+
+// init circle
+const circle = new Cotton('#circle')
+
